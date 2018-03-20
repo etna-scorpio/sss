5f2ebcde3762a3899e382c8222d9370809e9eedc
Alona <etna.scorpio@gmail.com>
2018-03-20 16:09:54 +0200

    Small fix for sticky-sidebar

--- src/js/sticky-sidebar.js
+++ src/js/sticky-sidebar.js
@@ -1,6 +1,7 @@
 import 'sticky-sidebar';
 
 new StickySidebar('#sticky-sidebar', {
+	containerSelector: '#main-content',
 	innerWrapperSelector: '.sidebar__inner',
 	topSpacing: 20,
 	bottomSpacing: 20,
