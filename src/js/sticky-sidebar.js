916ff5ad7e010286185b1f9f5cbeef49188b33d4
Alona <etna.scorpio@gmail.com>
2018-03-27 23:13:54 +0300

    Update images and add one more useful link

--- src/js/sticky-sidebar.js
+++ src/js/sticky-sidebar.js
@@ -1,9 +1,13 @@
 import 'sticky-sidebar';
 
-new StickySidebar('#sticky-sidebar', {
-	containerSelector: '#main-content',
-	innerWrapperSelector: '.sidebar__inner',
-	topSpacing: 20,
-	bottomSpacing: 20,
-	minWidth: 767
-});
+var sidebar = document.getElementById('sticky-sidebar');
+
+if (sidebar) {
+	new StickySidebar('#sticky-sidebar', {
+		containerSelector: '#main-content',
+		innerWrapperSelector: '.sidebar__inner',
+		topSpacing: 20,
+		bottomSpacing: 20,
+		minWidth: 767
+	});
+}
